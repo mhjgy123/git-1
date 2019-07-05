d826b950d836afa4c21a846a2250998bde9f732b
mhj <1226162368@qq.com>
2019-07-05 18:56:26 +0800

    hello

--- src/com/git/hello/Hello.java
+++ src/com/git/hello/Hello.java
@@ -7,7 +7,7 @@ package com.git.hello;
  */
 public class Hello {
      public void hello(){
-    	 System.out.println("edit by mhjgy567***");
-
+    	 System.out.println("edit by mhjgy567***123");
+                         
      }
 }
