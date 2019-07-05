f251a0596a9b0e6f111ce65b5dd7c9bdf180d96c
mhj <1226162368@qq.com>
2019-07-05 15:46:06 +0800

    gitlab

--- src/com/git/hello/Hello.java
+++ src/com/git/hello/Hello.java
@@ -1,5 +1,10 @@
 package com.git.hello;
 
+/*
+ *  gitlab 需要使用8080端口，其它程序不能占用，否则就是502错误
+ *  
+ *  更改默认8080端口
+ */
 public class Hello {
      public void hello(){
     	 System.out.println("edit by mhjgy567***");
