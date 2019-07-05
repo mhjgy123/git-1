18b7bd59b5ee65ecf2809c7dda674c197f787e5c
mhj <1226162368@qq.com>
2019-07-05 19:45:05 +0800

    2

--- src/com/git/hello/Hello.java
+++ src/com/git/hello/Hello.java
@@ -7,6 +7,6 @@ package com.git.hello;
  */
 public class Hello {
      public void hello(){
-                     System.out.println(1);    
+                     System.out.println(2);    
      }
 }
